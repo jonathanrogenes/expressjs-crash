84c37458dad522cbaf88383cc7b861dc8e0a48dc
Jonathan Rogenes <jonathanrogenes@gmail.com>
2026-02-18 16:20:52 +0900

    switched to static web server

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const path = require('path')
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+// setup static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'about.html'))
-})
 
 app.listen(8000, () => console.log(`Server is running on port 8000`))
