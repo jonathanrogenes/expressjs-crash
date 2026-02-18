04a6ae450be21ef53b8a96d310a2c3a080ab2bb1
Jonathan Rogenes <jonathanrogenes@gmail.com>
2026-02-18 18:39:13 +0900

    CRUD - update action with put request

--- routes/posts.js
+++ routes/posts.js
@@ -40,8 +40,20 @@ router.post('/', (req, res) => {
   }
 
   posts.push(newPost)
-  
+
   res.status(201).json(posts)
+})
+
+// Update Post
+router.put('/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const post = posts.find((post) => post.id === id)
+  if (!post) {
+    return res.status(404).json({ message: `A post with the the id of ${id} was not found` })
+  }
+
+  post.title = req.body.title
+  res.status(202).json(posts)
 })
 
 export default router
