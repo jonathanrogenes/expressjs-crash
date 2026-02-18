342fe10e580127e3441cb523dd6c10da3665b3c4
Jonathan Rogenes <jonathanrogenes@gmail.com>
2026-02-18 18:15:55 +0900

    routing files

--- server.js
+++ server.js
@@ -1,37 +1,16 @@
 const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 8000
+const posts = require('./routes/posts')
 
 const app = express()
 
 // Setup static folder
 // app.use(express.(path.join(__dirname, 'public')))
 
-let posts = [
-  { id: 1, title: 'Post one'},
-  { id: 2, title: 'Post two'},
-  { id: 3, title: 'Post three'}
-]
-// get all posts
-app.get('/api/posts', (req, res) => {
-  const limit = parseInt(req.query.limit)
+// Routes
+app.use('/api/posts', posts)
 
-  if (!isNaN(limit) && limit > 0) {
-    return res.status(200).json(posts.slice(0, limit))
-  }
-    res.status(200).json(posts)
 
-})
-
-// Get a single post
-app.get('/api/posts/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  const post = posts.find((post) => post.id === id)
-  if (!post) {
-   return res.status(404).json({ msg: `A post with the id of ${id} was not found` })
-  }
-    res.status(200).json(post)
-
-})
 
 app.listen(8000, () => console.log(`Server is running on port ${port}`))
