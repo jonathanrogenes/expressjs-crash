74d26f7d5f57ebfcac733564eec332bc5cd4faad
Jonathan Rogenes <jonathanrogenes@gmail.com>
2026-02-18 16:38:50 +0900

    api call with json

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const path = require('path')
 const app = express()
 
 // setup static folder
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.(path.join(__dirname, 'public')))
 
+let posts = [
+  { id: 1, title: 'Post one'},
+  { id: 2, title: 'Post two'},
+  { id: 3, title: 'Post three'}
+]
+app.get('/api/posts', (req, res) => {
+  res.json(posts)
+})
 
 app.listen(8000, () => console.log(`Server is running on port 8000`))
